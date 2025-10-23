866949b6b9ac4075e3be8dc0fc1925e040daa1fb
ClaytonDewey <clay@dryadmedia.com>
2025-10-23 13:59:59 -0500

    create: Setup and style color buttons

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
+import { Button } from './components';
+
 function App() {
   return (
     <div className='App'>
-      <div>
-        Pick player 1's mark
-        <br />
-        Remember: X goes first
-        <br />
-        <br />
-        New Game (vs CPU)
-        <br />
-        New Game (vs player)
-        <br />
-        <br />
-        {/* New game menu end */}
-        {/* Game board start */}
-        {/* x/o icon   */} turn
-        <br />
-        X (You)
-        <br /> {/* Your score */}
-        Ties
-        <br /> {/* Ties score*/}
-        X (CPU)
-        <br /> {/* CPU score*/}
-        Oh no, you lost
-        <br />
-        You won!
-        <br />
-        Player {/* 1/2*/} wins!
-        <br />
-        {/* x/o icon*/} takes the round
-        <br />
-        Round tied
-        <br />
-        Restart game?
-        <br />
-        Quit
-        <br />
-        Next round
-        <br />
-        No, cancel
-        <br />
-        Yes, restart
-        <br />
-      </div>
+      <Button className='btn btn-yellow'>Button 1</Button>
+      <Button className='btn btn-blue'>Button 2</Button>
     </div>
   );
 }
