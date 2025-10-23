7aca3439d827aa43a07d00d123f442c11360bfe0
ClaytonDewey <clay@dryadmedia.com>
2025-10-23 13:29:48 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        Pick player 1's mark
+        <br />
+        Remember: X goes first
+        <br />
+        <br />
+        New Game (vs CPU)
+        <br />
+        New Game (vs player)
+        <br />
+        <br />
+        {/* New game menu end */}
+        {/* Game board start */}
+        {/* x/o icon   */} turn
+        <br />
+        X (You)
+        <br /> {/* Your score */}
+        Ties
+        <br /> {/* Ties score*/}
+        X (CPU)
+        <br /> {/* CPU score*/}
+        Oh no, you lost
+        <br />
+        You won!
+        <br />
+        Player {/* 1/2*/} wins!
+        <br />
+        {/* x/o icon*/} takes the round
+        <br />
+        Round tied
+        <br />
+        Restart game?
+        <br />
+        Quit
+        <br />
+        Next round
+        <br />
+        No, cancel
+        <br />
+        Yes, restart
+        <br />
+      </div>
     </div>
   );
 }
