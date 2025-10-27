ed5e5150e025930161970b050a428c80f27d7877
ClaytonDewey <clay@dryadmedia.com>
2025-10-27 12:18:18 -0500

    feat: Add modal for game over stats

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -1,7 +1,11 @@
-import { GameTile } from '.';
+import { Button, GameTile, Modal } from '.';
+import { Icon } from '../svg';
 import { calculateStatus, calculateTurns, calculateWinner } from '../utils';
+import { useState } from 'react';
 
 const GameGrid = ({ xIsNext, squares, onPlay }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const toggleModal = () => setIsModalVisible(!isModalVisible);
   const winner = calculateWinner(squares);
   const turns = calculateTurns(squares);
   const player = xIsNext ? 'X' : 'O';
@@ -14,16 +18,52 @@ const GameGrid = ({ xIsNext, squares, onPlay }) => {
     onPlay(nextSquares);
   };
 
+  const renderContent = () => {
+    return (
+      <>
+        <div className='modal__content-text'>
+          <h2 className={`ie ${winner === 'X' ? 'yellow' : 'blue'}`}>
+            <Icon name={winner} /> takes the round
+          </h2>
+        </div>
+      </>
+    );
+  };
+
+  const renderActions = () => {
+    return (
+      <div className='btn-container'>
+        <Button onClick={toggleModal} className='btn btn-secondary'>
+          Quit
+        </Button>
+        <Button onClick={toggleModal} className='btn btn-yellow'>
+          Next Round
+        </Button>
+      </div>
+    );
+  };
+
   return (
-    <div className='game__board-grid'>
-      {squares.map((square, squareIndex) => (
-        <GameTile
-          key={squareIndex}
-          value={square}
-          onSquareClick={() => handleClick(squareIndex)}
+    <>
+      <p>{status}</p>
+      <div className='game__board-grid'>
+        {squares.map((square, squareIndex) => (
+          <GameTile
+            key={squareIndex}
+            value={square}
+            onSquareClick={() => handleClick(squareIndex)}
+          />
+        ))}
+      </div>
+      {isModalVisible && (
+        <Modal
+          title={status}
+          content={renderContent()}
+          actions={renderActions()}
+          onDismiss={() => toggleModal()}
         />
-      ))}
-    </div>
+      )}
+    </>
   );
 };
 export default GameGrid;
