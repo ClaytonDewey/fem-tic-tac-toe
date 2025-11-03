d8bf82bedc1aba0f6f6f0e1522a3faac3fb803f5
ClaytonDewey <clay@dryadmedia.com>
2025-11-03 14:01:32 -0600

    feat: Add quit game modal

--- src/components/GameHeader.jsx
+++ src/components/GameHeader.jsx
@@ -1,20 +1,66 @@
+import { useState } from 'react';
 import { Icon } from '../svg';
-import { Button } from '.';
+import { Button, Modal } from '.';
 import useGameStore from '../store/useGameStore';
 
 const GameHeader = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+  const { quitSession } = useGameStore();
   const currentPlayer = useGameStore((state) => (state.xIsNext ? 'X' : 'O'));
-  const { undoMove } = useGameStore();
-  return (
-    <header className='header'>
-      <Icon name='logo' />
-      <div className='turn'>
-        <Icon name={currentPlayer} /> Turn
+
+  const renderContent = () => {
+    return (
+      <>
+        <div className='modal__content-text'>
+          <h3>Restart Game?</h3>
+        </div>
+      </>
+    );
+  };
+
+  const handleQuit = () => {
+    quitSession();
+    toggleVisibility();
+  };
+
+  const renderActions = () => {
+    return (
+      <div className='btn-container'>
+        <Button
+          type='button'
+          className='btn btn-secondary'
+          onClick={toggleVisibility}>
+          No, Cancel
+        </Button>
+        <Button type='button' className='btn btn-yellow' onClick={handleQuit}>
+          Yes, Restart
+        </Button>
       </div>
-      <Button type='button' className='btn btn-reset' onClick={undoMove}>
-        <Icon name='restart' />
-      </Button>
-    </header>
+    );
+  };
+
+  return (
+    <>
+      <header className='header'>
+        <Icon name='logo' />
+        <div className='turn'>
+          <Icon name={currentPlayer} /> Turn
+        </div>
+        <Button
+          type='button'
+          className='btn btn-reset'
+          onClick={toggleVisibility}>
+          <Icon name='restart' />
+        </Button>
+      </header>
+
+      {isVisible && (
+        <Modal content={renderContent()} actions={renderActions()} />
+      )}
+    </>
   );
 };
 export default GameHeader;
